083d274d524f55d6350494673b26dc23e45fb775
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-07-29 10:04:01 -0700

    Initial commit

---
+++ src/objects/date.rs
@@ -0,0 +1,18 @@
+use rcgen::date_time_ymd;
+use time::OffsetDateTime;
+
+#[derive(Debug, Copy, Clone, Default)]
+pub struct Date {
+    /// flutter_rust_bridge:non_final
+    pub year: i32,
+    /// flutter_rust_bridge:non_final
+    pub month: u8,
+    /// flutter_rust_bridge:non_final
+    pub day: u8,
+}
+
+impl From<Date> for OffsetDateTime {
+    fn from(Date { year, month, day }: Date) -> Self {
+        date_time_ymd(year, month, day)
+    }
+}
